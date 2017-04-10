1702b2fd33a4535d0708cb3a0c4e978fe3af0717
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-04-10 13:57:19 -0600

    I made an index.php page and basic html layout added a few paragraphs and a picture

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+			About Jabari
+		</title>
+	</head>
+	<body>
+		<p> This is a page about me!</p>
+		<p>My name is Jabari and I am a Bootcamp Coder!</p>
+		<img src="images/bootcamp.img.png" alt="coders" height="222" width="185"/>
+	</body>
+</html>
