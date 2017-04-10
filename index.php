e9d003c19f4fd62aac6b7975bf45245e4363ae9c
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-04-10 14:16:37 -0600

    tried to add some styling

--- index.php
+++ index.php
@@ -6,11 +6,13 @@
 		</title>
 	</head>
 	<body>
+		<h1>Jabari</h1>
 		<p> This is a page about me!</p>
 		<p>My name is Jabari and I am a Bootcamp Coder!</p>
 		<img src="images/bootcamp.img.png" alt="coders" height="222" width="185"/>
 		<p> Just a few things about me, I recently moved to Albuquerque about 6 months ago
 		<br> Currently I am a Surface Scientist and hope to make a career change with Bootcamp Coders
 			<br> Also, I am married and have two Jack Russell Terriers</p>
+
 	</body>
 </html>
